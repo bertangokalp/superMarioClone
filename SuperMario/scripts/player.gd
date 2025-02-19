6662e44ee32086b69e11393d73e87cb1a064d1b2
Bertan Gokalp <52650844+bertangokalp@users.noreply.github.com>
2025-02-19 17:23:30 +0300

    Part 2

--- SuperMario/scripts/player.gd
+++ SuperMario/scripts/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 @export var speed: float = 150.0 #Karakterin yatay hareket hızı
 @export var jump_force: float = 300.0 #Zıplama Kuvveti
+
+@onready var anim = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
 var gravity = ProjectSettings.get("physics/2d/default_gravity") #Yerçekimi değerini proje ayarlarından çektim
 
 func _physics_process(delta):
-	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
@@ -15,6 +18,17 @@ func _physics_process(delta):
 		velocity.x = direction * speed #bastığı yöne git
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed) #Eğer yön girdisi yoksa dur
+
+	if direction != 0:
+		sprite.flip_h = direction < 0
+	
+	if is_on_floor():
+		if direction != 0:
+			anim.play("run")
+		else:
+			anim.play("idle")
+	else:
+		anim.play("jump")
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force #karakteri yukarı doğru fırlat
